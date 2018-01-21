bf14c3c2605cb54ec911ca5785430b1cc703e65f
Mark Topper <mark@ulties.com>
2018-01-21 13:23:24 +0100

    Fix "Class migrator does not exist"

--- src/Hooks.php
+++ src/Hooks.php
@@ -4,6 +4,7 @@ namespace Larapack\Hooks;
 
 use Carbon\Carbon;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Database\Migrations\Migrator;
 use Symfony\Component\Console\Input\ArrayInput;
 
 class Hooks
@@ -11,6 +12,7 @@ class Hooks
     protected static $remote = 'https://larapack.io';
 
     protected $filesystem;
+    protected $migrator;
     protected $hooks;
     protected $lastRemoteCheck;
     protected $outdated = [];
@@ -29,9 +31,11 @@ class Hooks
 
     protected static $fakeDateTime = false;
 
-    public function __construct(Filesystem $filesystem)
+    public function __construct(Filesystem $filesystem, Migrator $migrator)
     {
         $this->filesystem = $filesystem;
+        $this->migrator = $migrator;
+
         $this->prepareComposer();
         $this->readOutdated();
         $this->readJsonFile();
@@ -319,7 +323,7 @@ class Hooks
      *
      * @return bool
      */
-    public function update($name, $version = null, $migrate = true, $seed = true, $publish = true, $forcePublishing)
+    public function update($name, $version, $migrate, $seed, $publish, $forcePublishing)
     {
         // Check if hook exists
         if (!$this->downloaded($name)) {
@@ -495,10 +499,10 @@ class Hooks
     protected function makeStubFiles($name)
     {
         $replaces = [
-            'kebab-case' => $name,
-            'snake_case' => snake_case(str_replace('-', '_', $name)),
-            'camelCase' => camel_case(str_replace('-', '_', $name)),
-            'StudlyCase' => studly_case(str_replace('-', '_', $name)),
+            'kebab-case'          => $name,
+            'snake_case'          => snake_case(str_replace('-', '_', $name)),
+            'camelCase'           => camel_case(str_replace('-', '_', $name)),
+            'StudlyCase'          => studly_case(str_replace('-', '_', $name)),
             'MIGRATION_DATE_TIME' => $this->migrationDateTimeString(),
         ];
 
@@ -891,31 +895,31 @@ class Hooks
     /**
      * Run migrations found for a specific hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      */
     protected function migrateHook(Hook $hook)
     {
         $migrations = (array) $hook->getComposerHookKey('migrations', []);
 
-        app('migrator')->run($this->realPath($migrations, $hook->getPath().'/')->all());
+        $this->migrator->run($this->realPath($migrations, $hook->getPath().'/')->all());
     }
 
     /**
      * Rollback migrations found for a specific hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      */
     protected function unmigrateHook(Hook $hook)
     {
         $migrations = (array) $hook->getComposerHookKey('migrations', []);
 
-        app('migrator')->reset($this->realPath($migrations, $hook->getPath().'/')->all());
+        $this->migrator->reset($this->realPath($migrations, $hook->getPath().'/')->all());
     }
 
     /**
      * Run seeders found for a specific hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      */
     protected function seedHook(Hook $hook)
     {
@@ -928,7 +932,7 @@ class Hooks
     /**
      * Run unseeders found for a specific hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      */
     protected function unseedHook(Hook $hook)
     {
@@ -941,7 +945,7 @@ class Hooks
     /**
      * Publish assets found for a specific hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      */
     protected function publishHook(Hook $hook, $force = false)
     {
@@ -983,7 +987,7 @@ class Hooks
 
             $newFiles->merge($updatedFiles)
                 ->each(function ($filename) use ($realLocation, $publishPath, $filesystem) {
-                    $directory = substr($publishPath.'/'.$filename, 0, - strlen(basename($filename)));
+                    $directory = substr($publishPath.'/'.$filename, 0, -strlen(basename($filename)));
 
                     if (!$filesystem->isDirectory($directory)) {
                         $filesystem->makeDirectory($directory, 0755, true, true);
@@ -1001,7 +1005,7 @@ class Hooks
     /**
      * Unpublish assets found for a specific hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      */
     protected function unpublishHook(Hook $hook)
     {
@@ -1033,8 +1037,8 @@ class Hooks
     /**
      * Run seeder files.
      *
-     * @param array   $folders
-     * @param string  $basePath
+     * @param array  $folders
+     * @param string $basePath
      */
     protected function runSeeders($folders, $basePath)
     {
@@ -1046,9 +1050,9 @@ class Hooks
                     return $file->getExtension() == 'php';
                 })->each(function ($file) {
                     $class = substr($file->getFilename(), 0, -4);
-                    require_once($file->getRealPath());
+                    require_once $file->getRealPath();
 
-                    (new $class)->run();
+                    (new $class())->run();
                 });
             });
     }
@@ -1056,8 +1060,8 @@ class Hooks
     /**
      * Get collection of realpath paths.
      *
-     * @param array   $paths
-     * @param string  $basePath
+     * @param array  $paths
+     * @param string $basePath
      *
      * @return \Illuminate\Support\Collection
      */
@@ -1071,9 +1075,9 @@ class Hooks
     }
 
     /**
-     * Make temponary backup of hook
+     * Make temponary backup of hook.
      *
-     * @param \Larapack\Hooks\Hook  $hook
+     * @param \Larapack\Hooks\Hook $hook
      *
      * @return void
      */
