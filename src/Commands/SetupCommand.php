cf57ebbba9a45c39f2de2c451dedb0baf2e6192a
Tony Lea <tony@tonys-mbp-2.home>
2019-08-30 13:08:22 -0700

    updating helpers

--- src/Commands/SetupCommand.php
+++ src/Commands/SetupCommand.php
@@ -4,6 +4,7 @@ namespace Larapack\Hooks\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
 use Larapack\Hooks\Composer;
 use Larapack\Hooks\Events\Setup;
 use Larapack\Hooks\HooksServiceProvider;
@@ -39,7 +40,7 @@ class SetupCommand extends Command
             'url'  => $this->option('url'),
         ]);
 
-        if (starts_with($this->option('url'), 'http://')) {
+        if (Arr::startsWith($this->option('url'), 'http://')) {
             $composer->addConfig('secure-http', false);
         }
 
